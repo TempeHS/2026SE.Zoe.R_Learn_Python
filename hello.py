fcad17fdd0f7db1b3418ff28aa6e8c7322eca006
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-02-06 23:30:59 +0000

    started the lecture

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print ("hello, world")
+# zoe was here
+name = input ("What's your name? ")
+
+# removes whitespace from string and capitalises name
+name = name.strip().title()
+
+# split user's name into first name and last name
+first, last, what = name.split(" ")
+
+print (f"hello, {first}")
