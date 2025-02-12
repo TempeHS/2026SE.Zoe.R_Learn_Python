022a9a236491ab5c5017bffa272089586abb9f83
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-02-12 02:50:51 +0000

    finished faces.py

---
+++ 0-FunctionsVariables/faces/faces.py
@@ -0,0 +1,9 @@
+#i personally think this problem is ridiculous.
+#emoticons are FAR superior to emojis, and i WILL die on this hill.
+
+moticon = input ("SUPERIOR emoticon: ")
+
+moticon = moticon.replace(":)", "🙂")
+moticon = moticon.replace(":(", "🙁")
+
+print (f"here is your STINKY emoji - {moticon}")
