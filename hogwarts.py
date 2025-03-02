778e4cb035c0cb15fcdfbfa1a8d464046d00d8da
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2025-03-02 07:14:40 +0000

    finished nutrition.py

--- hogwarts.py
+++ hogwarts.py
@@ -9,10 +9,10 @@ students = [
 for student in students:
     print(student["name"], student["house"], student["patronus"], sep = ", ")
 
-#students = ["Hermione", "Harry", "Ron"]
+students = ["Hermione", "Harry", "Ron"]
 
-#for student in students:
-#    print(student)
+for student in students:
+    print(student)
 
-#for i in range(len(students)):
-#    print(i + 1, students[i])
+for i in range(len(students)):
+    print(i + 1, students[i])
